9f64bc787b01871f081469308c25dc8bb904b1bb
PKingo <philipandreaskingo@hotmail.com>
2019-10-20 09:41:54 +0200

    Added env. variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,18 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { API_KEY, PROJECT_ID } from '../../keys';
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: `${PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${PROJECT_ID}.firebaseio.com`,
-  projectId: PROJECT_ID,
-  storageBucket: `${PROJECT_ID}.appspot.com`,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'braetspilsministeriet-fe76f.firebaseapp.com',
+  databaseURL: 'https://braetspilsministeriet-fe76f.firebaseio.com',
+  projectId: 'braetspilsministeriet-fe76f',
+  storageBucket: 'braetspilsministeriet-fe76f.appspot.com',
   messagingSenderId: '1015370273143',
   appId: '1:1015370273143:web:cb6e5e094d178d10ba1172'
 };
+
+console.log('Firebase config', firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
